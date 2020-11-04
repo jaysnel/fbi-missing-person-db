36f8849ff3ec76e53a095c67ced8113be2fec2ff
jaylan <jaylan210@gmail.com>
2020-11-03 21:28:43 -0500

    db set up, clears DB before adding data list, cleaned up a little

--- serve.js
+++ serve.js
@@ -1,22 +1,23 @@
 //DB Info
-// const mongodb = require('mongodb');
-// const mongoclient = mongodb.MongoClient;
-// const connectionURL = 'mongodb://127.0.0.1:27017';
-// const databasename = 'fbi-missing-persons';
+const mongodb = require('mongodb');
+const mongoclient = mongodb.MongoClient;
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databasename = 'fbi-missing-persons';
+const nameofcollection = 'missing-persons';
 
 //Web Scraping Info
 const puppeteer = require('puppeteer');
-let fs = require('fs');
+const fs = require('fs');
 const fbiurl = "https://www.fbi.gov/wanted/kidnap";
 let missingpersons = [];
-let fbimissingpersondatafileName = "fbimissingpersondata.txt";
-let fbimissingpersondetailsfileName = "fbimissingpersondetailsfile.txt"
+const fbimissingpersondatafileName = "fbimissingpersondata.txt";
+const fbimissingpersondetailsfileName = "fbimissingpersondetailsfile.txt"
 let fbimissingpersondetails = [];
 
 // function for geting intial links of missing persons from fbi url
 let storeInitialFBIData = async () => {
-    //const browser = await puppeteer.launch({headless: false});
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
+    //const browser = await puppeteer.launch();
     const page = await browser.newPage();
   
     await page.goto(fbiurl);
@@ -41,6 +42,7 @@ let storeInitialFBIData = async () => {
         return person.replace(/\n/g, "");
     }
 
+    //maybe theres a better way than writing to a file, but for now this works and gets the job done
     fs.writeFile(fbimissingpersondatafileName, JSON.stringify(missingpersons), err => {
         if(err) console.log(err);
     })
@@ -50,18 +52,23 @@ let storeInitialFBIData = async () => {
 
 // function for geting details of each missing person from fbi missing person url
   let getPersonsFromFBIData = async (link) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
+    //const browser = await puppeteer.launch();
     const page = await browser.newPage();
     let missingpersonobj = {};
   
     await page.goto(link, { waitUntil: 'networkidle0' });
 
     let name = await page.evaluate(() => document.querySelector('.documentFirstHeading').textContent);
+    let image = await page.evaluate(() => document.querySelector('.wanted-person-mug img').getAttribute('src'));
     let submittip = await page.evaluate(() => document.querySelector('.wanted-person-submit p').textContent);
     let details = await page.evaluate(() => document.querySelector('.wanted-person-details p').textContent);
     let content = await page.evaluate(() => document.querySelector('.wanted-person-description table tbody tr'));
+    //fbi data for each person was inside of a table that had no ids or classes,
+    //but were the only time they used a table at all on the page
     let allmissingpersoninfo = await page.evaluate(() => Array.from(content.querySelectorAll('td'), element => element.innerHTML));
     
+    //creating object of each persons data and pushing it to a global array
     let newcontent = [];
     for(var i = 0; i < allmissingpersoninfo.length; i += 2) {
         newcontent.push(allmissingpersoninfo.slice(i, i + 2));
@@ -72,27 +79,37 @@ let storeInitialFBIData = async () => {
     missingpersonobj["Name"] = name;
     missingpersonobj["Details"] = details;
     missingpersonobj["Submit a Tip"] = submittip;
-
-    // need to send info gathered to a DB here
+    missingpersonobj["Found"] = false;
+    missingpersonobj["Headshot"] = image;
+    //pushing to global array
     fbimissingpersondetails.push(missingpersonobj);
 
-    // mongoclient.connect(connectionURL, { useUnifiedTopology: true }, (err, client) => {
-    //     if(err) return console.log(err);
+    mongoclient.connect(connectionURL, { useUnifiedTopology: true }, (err, client) => {
+        if(err) return console.log(err);
         
-    //     const db = client.db(databasename);
-        
-        //Updating if already exists
+        const db = client.db(databasename);
 
-        
-        //Inserting each missing person to DB
-    //     db.collection('missing-persons').insertOne(missingpersonobj, (err, res) => {
-    //         if(err) return console.log(err);
-    //         console.log(res.ops);
-    //     });
+        fs.readFile(fbimissingpersondatafileName, (err, data) => {
+            if(err) return console.log(err);
+            let details = JSON.parse(data);
+            if(details.length === fbimissingpersondetails.length) {
+                //Clear DB out here
+                db.collection(nameofcollection).deleteMany({
+                    Found: false
+                }, (err) => {
+                    if(err) return console.log(err);
+                })
 
-    //     console.log("Saved Person To DB.");
-    // })
-
+                //Inserting each missing person to DB
+                db.collection(nameofcollection).insertMany(fbimissingpersondetails, (err) => {
+                    if(err) return console.log(err);
+                    return false;
+                });
+                console.log("Saved Missing Persons To DB.");
+            }
+        })
+    })
+    //this makes sense right?
     fs.writeFile(fbimissingpersondetailsfileName, JSON.stringify(fbimissingpersondetails), err => {
         if(err) console.log(err);
         console.log("Saved Person.");
