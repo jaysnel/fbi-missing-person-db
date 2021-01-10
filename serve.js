aa1160b0091ef6299707d689b11a2f453fc3ae2c
root <jaylan210@gmail.com>
2021-01-10 16:59:50 -0500

    adding twilio, undoing merge manually

--- serve.js
+++ serve.js
@@ -1,12 +1,5 @@
 //DB Info
 require('dotenv').config();
-//General data
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
 
 //MongoDB data
 const mongodb = require('mongodb');
@@ -148,55 +141,3 @@ function sendTextMessage(err) {
 
 
 getalldata();
-let client;
-
-//Connection data
-let app = express();
-let port = process.env.PORT || 8080;
-
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan('combined'));
-
-//Main Home
-app.get('/', (req, res) => {
-
-     return res.send("Welcome To Missing Person DB.");
-});
-
-
-//Return All Data
-app.get('/v1/all', (req, res) => {
-
-     let getFullList = async() => {
-
-     returnNewClientInstance();
-     client.connect(async(err) => {
-          if(err) return catchError(err);
-
-          const collection = await client.db(databasename).collection(nameofcollection);
-          
-          let data = await collection.find({}, { projection: { _id: 0 } }).toArray();
-          res.send(data);
-          client.close();
-          });
-     
-     }
-
-     getFullList();
-});
-
-//Other Functions
-//Re-initializing client, was not able to make this global as it never get reinitialized
-function returnNewClientInstance() {
-     return client = new mongoclient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
-}
-
-function catchError(err) {
-     console.log(err);
- }
-
-app.listen(port, function () {
-     console.log("Running on port " + port);
-});
